b2f8165823d8c8179aeb76cce632c5da0a58ba33
Carlos Avalos <carlosavsi@hotmail.com>
2018-04-24 10:23:28 -0500

    Login con detalle al ingresar usuario y contraseña inválidos

--- welcome.php
+++ welcome.php
@@ -1,12 +1,35 @@
 <?php
-if ( $_POST["user"] == "" && $_POST["contra"] == "")
+	@include("config.php");
+	@include("includes/head.php");
+
+	$usuario = $_POST["user"];
+	$contra = $_POST["contra"];
+
+	if($usuario == "" && $contra == "")
 	{
-		echo "ingresar usuario y contraseña";
+		echo "Ingrese usuario y contraseña por favor";
 	}
-else
+	else
 	{
-		echo "Bienvenido  ". $_POST["user"];
-	}
+		$sql->real_query("SELECT  `Nombre` FROM `usuarios` WHERE `Usuario` = '$usuario' and `Password` = '$contra'");
+		$resultado = $sql->use_result();
+
+		while ( $fila = $resultado->fetch_assoc())
+		{			
+			if (is_null($fila['Nombre']))
+			{
+				echo "Usuario y/o contraseña incorrectos";
+			}
+			else
+			{
+				echo "Bienvenido: " . $fila['Nombre'];
+			}
+		}
+	}		
 ?>
-<br>
-<a href="index.php"> Regresar </a>
+	<br>
+	<a href="index.php"> Regresar </a>
+
+<?php
+	@include("includes/footer.php");
+?>
